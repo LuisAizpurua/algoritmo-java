dba814421928d00217c06529e622619c1f8a3a45
Moises <moises@correo.com>
2025-04-22 10:24:42 -0500

    increase quick-sort

---
+++ src/main/java/principal/Home.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Home {
+
+
+    public static void main(final String[] args) {
+
+        int[] arr ={3,5,1,7,2,6,4};
+        new QuickSort(arr).runSort();
+    }
+}
